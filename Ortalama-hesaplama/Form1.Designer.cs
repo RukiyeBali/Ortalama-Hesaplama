d7287e4c936430b748f8b55a61c77459fb49e024
Rukiye Balı <145977861+RukiyeBali@users.noreply.github.com>
2023-10-15 21:56:27 +0300

    Add files via upload

---
+++ Ortalama-hesaplama/Form1.Designer.cs
@@ -0,0 +1,101 @@
+﻿namespace Ortalama_hesaplama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblOrtalama = new System.Windows.Forms.Label();
+            this.btnHesapla = new System.Windows.Forms.Button();
+            this.txtOrtalama = new System.Windows.Forms.TextBox();
+            this.lblSonuc = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblOrtalama
+            // 
+            this.lblOrtalama.AutoSize = true;
+            this.lblOrtalama.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblOrtalama.Location = new System.Drawing.Point(48, 63);
+            this.lblOrtalama.Name = "lblOrtalama";
+            this.lblOrtalama.Size = new System.Drawing.Size(82, 18);
+            this.lblOrtalama.TabIndex = 0;
+            this.lblOrtalama.Text = "Ortalama:";
+            // 
+            // btnHesapla
+            // 
+            this.btnHesapla.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnHesapla.Location = new System.Drawing.Point(136, 109);
+            this.btnHesapla.Name = "btnHesapla";
+            this.btnHesapla.Size = new System.Drawing.Size(114, 35);
+            this.btnHesapla.TabIndex = 1;
+            this.btnHesapla.Text = "Hesapla";
+            this.btnHesapla.UseVisualStyleBackColor = true;
+            this.btnHesapla.Click += new System.EventHandler(this.btnHesapla_Click);
+            // 
+            // txtOrtalama
+            // 
+            this.txtOrtalama.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.txtOrtalama.Location = new System.Drawing.Point(136, 60);
+            this.txtOrtalama.Name = "txtOrtalama";
+            this.txtOrtalama.Size = new System.Drawing.Size(114, 24);
+            this.txtOrtalama.TabIndex = 2;
+            // 
+            // lblSonuc
+            // 
+            this.lblSonuc.AutoSize = true;
+            this.lblSonuc.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblSonuc.Location = new System.Drawing.Point(69, 185);
+            this.lblSonuc.Name = "lblSonuc";
+            this.lblSonuc.Size = new System.Drawing.Size(61, 18);
+            this.lblSonuc.TabIndex = 3;
+            this.lblSonuc.Text = "Sonuc:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(305, 263);
+            this.Controls.Add(this.lblSonuc);
+            this.Controls.Add(this.txtOrtalama);
+            this.Controls.Add(this.btnHesapla);
+            this.Controls.Add(this.lblOrtalama);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblOrtalama;
+        private System.Windows.Forms.Button btnHesapla;
+        private System.Windows.Forms.TextBox txtOrtalama;
+        private System.Windows.Forms.Label lblSonuc;
+    }
+}
+
---
+++ Ortalama-hesaplama/Form1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics.Eventing.Reader;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ortalama_hesaplama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            lblSonuc.Text = "";
+        }
+
+        private void btnHesapla_Click(object sender, EventArgs e)
+        {
+            float ortalama;
+
+            ortalama=Convert.ToSingle(txtOrtalama.Text);
+
+            if (ortalama < 0)
+            {
+                lblSonuc.Text = "Negatif değer giremessiniz.";
+            }
+
+            else if (ortalama < 50)
+            {
+                lblSonuc.Text = "Kaldınız.";
+            }
+            else if (ortalama < 70)
+            {
+                lblSonuc.Text = "Tebrikler geçtiniz.";
+            }
+            else if (ortalama < 85)
+            {
+                lblSonuc.Text = "Teşkkür belgesi aldınız.";
+            }
+            else if (ortalama <= 100)
+            {
+                lblSonuc.Text = "Takdir belgesi aldınız.";
+
+            }
+        }
+    }
+}
